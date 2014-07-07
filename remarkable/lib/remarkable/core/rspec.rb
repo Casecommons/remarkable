066dc6bb0416925d35c2c24c4e8644ec8acd7fbe
Andrew Marshall and Nora Lin <pair+andrew+nora@pivotallabs.com>
2014-07-07 17:43:05 -0400

    Remove hacky xexample/xit/xspecify that are out of place
    They have nothing to do with the rest of the gem.

--- remarkable/lib/remarkable/core/rspec.rb
+++ remarkable/lib/remarkable/core/rspec.rb
@@ -8,24 +8,4 @@ module RSpec #:nodoc:
       "#{verb} #{last_description}"
     end
   end
-
-  module Core #:nodoc:
-    class ExampleGroup #:nodoc:
-      # Overwrites to provide I18n on example disabled message.
-      #
-      def _xexample(description=nil, opts={}, &block)
-        disabled = Remarkable.t 'remarkable.core.example_disabled', :default => 'Example disabled'
-        Kernel.warn("#{disabled}: #{description}")
-      end
-      #alias_method :xit, :xexample
-      #alias_method :xspecify, :xexample
-      
-      # NOTE: Hack. Disabled examples is not the same as pending examples
-      # However, the rspec runner is monolithic and doesn't have a hook to process
-      # examples based on metadata
-      alias_example_to :xexample, :disabled => true, :pending => true
-      alias_example_to :xit, :disabled => true, :pending => true
-      alias_example_to :xspecify, :disabled => true, :pending => true
-    end
-  end
 end
