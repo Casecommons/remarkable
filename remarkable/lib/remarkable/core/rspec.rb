23db91d681e67c07dbceb6a8fc5152fec87ba2c9
Andrew Marshall and Nora Lin <pair+andrew+nora@pivotallabs.com>
2014-07-08 12:15:53 -0400

    Remove probably not really needed and broken code

--- remarkable/lib/remarkable/core/rspec.rb
+++ remarkable/lib/remarkable/core/rspec.rb
@@ -2,10 +2,10 @@ module RSpec #:nodoc:
   module Matchers #:nodoc:
     # Overwrites to provide I18n on should and should_not.
     #
-    def self.generated_description
-      return nil if last_should.nil?
-      verb = Remarkable.t "remarkable.core.#{last_should}", :default => last_should.to_s.gsub('_',' ')
-      "#{verb} #{last_description}"
-    end
+    # def self.generated_description
+      # return nil if last_should.nil?
+      # verb = Remarkable.t "remarkable.core.#{last_should}", :default => last_should.to_s.gsub('_',' ')
+      # "#{verb} #{last_description}"
+    # end
   end
 end
